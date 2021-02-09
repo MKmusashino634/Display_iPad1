682f09188a198969655583f9fa55dd9fbbd04566
MKmusashino634 <shmakpp@outlook.com>
2021-02-09 18:01:28 +0900

    ページ切り替え
    らしくなってきた。

--- serve.js
+++ serve.js
@@ -15,10 +15,26 @@ fsオブジェクト、、、非同期！
 //アクセスをipで制限、したかった、、、
 var ips = [ '127.0.0.1' ];
 server.use( ipfilter( ips, { mode: 'allow' } ) );
+
+追記、ファイアウォールでアプリケーションの通信を許可しなければ良い！
 */
 
 var server = http.createServer(function(req, res){
-    fs.readFile('./serve.html', 'utf-8', function(err, data){
+    var target = '';
+    switch(req.url){
+        case '/':
+        case '/Display_iPad1-page1':
+            target = './serve1.html';
+            break;
+        case '/Display_iPad1-page2':
+            target = './serve2.html';
+            break;
+        default:
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('bad request');
+            return;
+    }
+    fs.readFile(target, 'utf-8', function(err, data){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         res.end();
