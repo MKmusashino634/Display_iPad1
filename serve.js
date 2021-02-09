10f7f9922c7dc6bea14b273ec6356e8b11380a72
MKmusashino634 <shmakpp@outlook.com>
2021-02-09 17:29:11 +0900

    2
    セキュリティ強化したい。
    tlsとip制限。
    今後の課題。

--- serve.js
+++ serve.js
@@ -1,9 +1,28 @@
 var http = require('http');
+var fs = require('fs');
+/*
+fsオブジェクト、、、非同期！
+第一引数　対象のファイルパス
+第二引数　エンコード
+第三引数　コールバック函数指定
+
+コールバック函数、、、
+第一引数　エラーオブジェクト
+第二引数　読み込んだデータ
+*/
+
+/*
+//アクセスをipで制限、したかった、、、
+var ips = [ '127.0.0.1' ];
+server.use( ipfilter( ips, { mode: 'allow' } ) );
+*/
 
 var server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World');
-    res.end();
+    fs.readFile('./serve.html', 'utf-8', function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 });
 
 //listenportは3000
