af14ed7f132c019150f4c0ae27331c203057b6eb
MKmusashino634 <shmakpp@outlook.com>
2021-02-09 18:14:11 +0900

    非同期から同期へ
    nodeを少しずつ理解。

--- serve.js
+++ serve.js
@@ -15,30 +15,35 @@ fsオブジェクト、、、非同期！
 //アクセスをipで制限、したかった、、、
 var ips = [ '127.0.0.1' ];
 server.use( ipfilter( ips, { mode: 'allow' } ) );
+※「use」がよくわからない状況。
 
-追記、ファイアウォールでアプリケーションの通信を許可しなければ良い！
+追記、ファイアウォールでアプリケーションの通信を許可しなければ良い
+と思ったが、windowsファイアウォールで設定するとLAN内でもできない。ループバックのみとなってしまう。
 */
+
+//予めすべてのファイルを読み込んで無駄をなくす。
+var Page1 = fs.readFileSync('./serve1.html', 'utf-8')
+var Page2 = fs.readFileSync('./serve2.html', 'utf-8')
 
 var server = http.createServer(function(req, res){
     var target = '';
     switch(req.url){
         case '/':
         case '/Display_iPad1-page1':
-            target = './serve1.html';
+            target = Page1;
             break;
         case '/Display_iPad1-page2':
-            target = './serve2.html';
+            target = Page2;
             break;
         default:
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end('bad request');
             return;
     }
-    fs.readFile(target, 'utf-8', function(err, data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-    });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(target);
+    res.end();
+    
 });
 
 //listenportは3000
