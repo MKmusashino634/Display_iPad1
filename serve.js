3342a60361ef947a323ad461717918e9b0331d11
MKmusashino634 <shmakpp@outlook.com>
2021-02-11 11:01:16 +0900

    データ授受
    readmeの変更も

--- serve.js
+++ serve.js
@@ -1,15 +1,50 @@
 var http = require('http');
 var fs = require('fs');
-/*
-fsオブジェクト、、、非同期！
-第一引数　対象のファイルパス
-第二引数　エンコード
-第三引数　コールバック函数指定
 
-コールバック函数、、、
-第一引数　エラーオブジェクト
-第二引数　読み込んだデータ
-*/
+//urlの文字列解析
+var url = require('url');
+
+//クエリ文字列を扱う
+var qs = require('querystring');
+
+
+//予めすべてのファイルを読み込んで無駄をなくす。
+var Page1 = fs.readFileSync('./serve1.html', 'utf-8');
+
+//
+var server = http.createServer(function(req, res){
+    if(req.method == 'GET'){
+        //urlをparseメソッドで分析してオブジェクト型の値として返す
+        var urlParts = url.parse(req.url,true)
+        console.log('---GET Request---');
+        console.log('nameは' + urlParts.query.name);
+        console.log('ageは' + urlParts.query.age);
+    }
+    else{
+        var body = '';
+        //データ受信後dataの登録
+        req.on('data', function(data){
+        //逐次結合
+            body += data;
+        });
+        req.on('end', function(){
+            var params = qs.parse(body);
+            console.log('---POST Request---');
+            console.log('nameは' + params.name);
+            console.log('ageは' + params.age);
+        });
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(Page1);
+    res.end();
+    
+});
+
+//listenportは3000
+server.listen(3000);
+console.log('　起動した！')
+
+
 
 /*
 //アクセスをipで制限、したかった、、、
@@ -20,32 +55,3 @@ server.use( ipfilter( ips, { mode: 'allow' } ) );
 追記、ファイアウォールでアプリケーションの通信を許可しなければ良い
 と思ったが、windowsファイアウォールで設定するとLAN内でもできない。ループバックのみとなってしまう。
 */
-
-//予めすべてのファイルを読み込んで無駄をなくす。
-var Page1 = fs.readFileSync('./serve1.html', 'utf-8')
-var Page2 = fs.readFileSync('./serve2.html', 'utf-8')
-
-var server = http.createServer(function(req, res){
-    var target = '';
-    switch(req.url){
-        case '/':
-        case '/Display_iPad1-page1':
-            target = Page1;
-            break;
-        case '/Display_iPad1-page2':
-            target = Page2;
-            break;
-        default:
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('bad request');
-            return;
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(target);
-    res.end();
-    
-});
-
-//listenportは3000
-server.listen(3000);
-console.log('　起動した！')
